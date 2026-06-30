7b1698bfaf7b03d5e02163bb8454b1389a785772
adelli-chandrashekar <adelli.chandrashekar@gmail.com>
2026-06-30 20:07:45 +0530

    Enable headless Chrome for CI/CD

--- conftest.py
+++ conftest.py
@@ -50,10 +50,14 @@ def driver(config):
     """
     logger.info("Starting Chrome Browser...")
     
-    # We use WebDriver Manager to automatically download the correct chromedriver!
     service = ChromeService(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")  # Uncomment to run invisibly!
+    
+    # We MUST run invisibly in CI/CD because there is no physical monitor!
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
     
     driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
